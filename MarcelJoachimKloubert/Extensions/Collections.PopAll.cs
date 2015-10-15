8b3ec4b9c84f94edaed1ab8c90d366a93586b451
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-10-15 22:01:59 +0200

    - ADD: PopAll() and PopOrDefault() methods

---
+++ MarcelJoachimKloubert/Extensions/Collections.PopAll.cs
@@ -0,0 +1,64 @@
+﻿/**********************************************************************************************************************
+ * Extensions.NET (https://github.com/mkloubert/Extensions.NET)                                                       *
+ *                                                                                                                    *
+ * Copyright (c) 2015, Marcel Joachim Kloubert <marcel.kloubert@gmx.net>                                              *
+ * All rights reserved.                                                                                               *
+ *                                                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   *
+ * following conditions are met:                                                                                      *
+ *                                                                                                                    *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the          *
+ *    following disclaimer.                                                                                           *
+ *                                                                                                                    *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+ *    following disclaimer in the documentation and/or other materials provided with the distribution.                *
+ *                                                                                                                    *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote    *
+ *    products derived from this software without specific prior written permission.                                  *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, *
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  *
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE   *
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.Extensions
+{
+    // PopAll()
+    static partial class MJKCoreExtensionMethods
+    {
+        #region Methods (1)
+
+        /// <summary>
+        /// Pops all items from a <see cref="Stack{T}" />.
+        /// </summary>
+        /// <typeparam name="T">Type of the items.</typeparam>
+        /// <param name="stack">The stack from where to pop the items from.</param>
+        /// <returns>The poped items as delayed sequence.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stack" /> is <see langword="null" />.
+        /// </exception>
+        public static IEnumerable<T> PopAll<T>(this Stack<T> stack)
+        {
+            if (stack == null)
+            {
+                throw new ArgumentNullException("stack");
+            }
+
+            while (stack.Count > 0)
+            {
+                yield return stack.Pop();
+            }
+        }
+
+        #endregion Methods (1)
+    }
+}
---
+++ MarcelJoachimKloubert/Extensions/Collections.PopOrDefault.cs
@@ -0,0 +1,83 @@
+﻿/**********************************************************************************************************************
+ * Extensions.NET (https://github.com/mkloubert/Extensions.NET)                                                       *
+ *                                                                                                                    *
+ * Copyright (c) 2015, Marcel Joachim Kloubert <marcel.kloubert@gmx.net>                                              *
+ * All rights reserved.                                                                                               *
+ *                                                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   *
+ * following conditions are met:                                                                                      *
+ *                                                                                                                    *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the          *
+ *    following disclaimer.                                                                                           *
+ *                                                                                                                    *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+ *    following disclaimer in the documentation and/or other materials provided with the distribution.                *
+ *                                                                                                                    *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote    *
+ *    products derived from this software without specific prior written permission.                                  *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, *
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  *
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE   *
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.Extensions
+{
+    // PopOrDefault()
+    static partial class MJKCoreExtensionMethods
+    {
+        #region Methods (2)
+
+        /// <summary>
+        /// Tries to pop the most upper value of a <see cref="Stack{T}" />.
+        /// </summary>
+        /// <typeparam name="T">Type of the items of the stack.</typeparam>
+        /// <param name="stack">The stack.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The most upper value or the default value from <paramref name="defaultValue" />.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stack" /> is <see langword="null" />.
+        /// </exception>
+        public static T PopOrDefault<T>(this Stack<T> stack, T defaultValue = default(T))
+        {
+            return PopOrDefault<T>(stack,
+                                   (s) => defaultValue);
+        }
+
+        /// <summary>
+        /// Tries to pop the most upper value of a <see cref="Stack{T}" />.
+        /// </summary>
+        /// <typeparam name="T">Type of the items of the stack.</typeparam>
+        /// <param name="stack">The stack.</param>
+        /// <param name="defaultValueProvider">The function / method that provides the default value.</param>
+        /// <returns>The most upper value or the default value from <paramref name="defaultValueProvider" />.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stack" /> and/or <paramref name="defaultValueProvider" /> are <see langword="null" />.
+        /// </exception>
+        public static T PopOrDefault<T>(this Stack<T> stack, Func<Stack<T>, T> defaultValueProvider)
+        {
+            if (stack == null)
+            {
+                throw new ArgumentNullException("stack");
+            }
+
+            if (defaultValueProvider == null)
+            {
+                throw new ArgumentNullException("defaultValueProvider");
+            }
+
+            return stack.Count > 0 ? stack.Pop() : defaultValueProvider(stack);
+        }
+
+        #endregion Methods (1)
+    }
+}
