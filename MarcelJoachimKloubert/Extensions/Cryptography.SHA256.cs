a0313c1443c4b8291af8869a29a006d0b76155c4
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-10-16 20:41:32 +0200

    - ADD: MD5() and SHA(256|384|512)() methods

---
+++ MarcelJoachimKloubert/Extensions/Cryptography.SHA256.cs
@@ -0,0 +1,90 @@
+﻿/**********************************************************************************************************************
+ * Extensions.NET (https://github.com/mkloubert/Extensions.NET)                                                       *
+ *                                                                                                                    *
+ * Copyright (c) 2015, Marcel Joachim Kloubert <marcel.kloubert@gmx.net>                                              *
+ * All rights reserved.                                                                                               *
+ *                                                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   *
+ * following conditions are met:                                                                                      *
+ *                                                                                                                    *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the          *
+ *    following disclaimer.                                                                                           *
+ *                                                                                                                    *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+ *    following disclaimer in the documentation and/or other materials provided with the distribution.                *
+ *                                                                                                                    *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote    *
+ *    products derived from this software without specific prior written permission.                                  *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, *
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  *
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE   *
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace MarcelJoachimKloubert.Extensions
+{
+    // SHA256()
+    static partial class MJKCoreExtensionMethods
+    {
+        #region Methods (2)
+
+        /// <summary>
+        /// Hashes binary data with SHA-256.
+        /// </summary>
+        /// <param name="data">The data to hash.</param>
+        /// <returns>The hash or <see langword="null" /> if <paramref name="data" /> is <see langword="null" />.</returns>
+        public static byte[] SHA256(this IEnumerable<byte> data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            var blob = data as byte[];
+            if (blob == null)
+            {
+                blob = data.ToArray();
+            }
+
+            using (var temp = new MemoryStream(blob, false))
+            {
+                return SHA256(temp);
+            }
+        }
+
+        /// <summary>
+        /// Hashes a stream with SHA-256.
+        /// </summary>
+        /// <param name="stream">The stream to hash.</param>
+        /// <returns>The hash.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stream" /> is <see langword="null" />.
+        /// </exception>
+        public static byte[] SHA256(this Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            using (var hasher = new SHA256CryptoServiceProvider())
+            {
+                return hasher.ComputeHash(stream);
+            }
+        }
+
+        #endregion Methods (2)
+    }
+}
